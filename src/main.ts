08ca4be289a44984628687f60c81a77ef223235c
dadwadw233 <913217005@qq.com>
2022-12-18 04:40:28 +0800

    还在写关系网络，暂存

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 
+
 // 注册elementplus图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,115 +1,9 @@
 <template>
 	<div>
 		<el-row :gutter="20">
-			<el-col :span="8">
-				<el-card shadow="hover" class="mgb20" style="height: 252px">
-					<div class="user-info">
-						<el-avatar :size="120" :src="imgurl" />
-						<div class="user-info-cont">
-							<div class="user-info-name">{{ name }}</div>
-							<div>{{ role }}</div>
-						</div>
-					</div>
-					<div class="user-info-list">
-						上次登录时间：
-						<span>2022-10-01</span>
-					</div>
-					<div class="user-info-list">
-						上次登录地点：
-						<span>东莞</span>
-					</div>
-				</el-card>
-				<el-card shadow="hover" style="height: 252px">
-					<template #header>
-						<div class="clearfix">
-							<span>语言详情</span>
-						</div>
-					</template>
-					Vue
-					<el-progress :percentage="79.4" color="#42b983"></el-progress>
-					TypeScript
-					<el-progress :percentage="14" color="#f1e05a"></el-progress>
-					CSS
-					<el-progress :percentage="5.6"></el-progress>
-					HTML
-					<el-progress :percentage="1" color="#f56c6c"></el-progress>
-				</el-card>
-			</el-col>
-			<el-col :span="16">
-				<el-row :gutter="20" class="mgb20">
-					<el-col :span="8">
-						<el-card shadow="hover" :body-style="{ padding: '0px' }">
-							<div class="grid-content grid-con-1">
-								<el-icon class="grid-con-icon"><User /></el-icon>
-								<div class="grid-cont-right">
-									<div class="grid-num">1234</div>
-									<div>用户访问量</div>
-								</div>
-							</div>
-						</el-card>
-					</el-col>
-					<el-col :span="8">
-						<el-card shadow="hover" :body-style="{ padding: '0px' }">
-							<div class="grid-content grid-con-2">
-								<el-icon class="grid-con-icon"><ChatDotRound /></el-icon>
-								<div class="grid-cont-right">
-									<div class="grid-num">321</div>
-									<div>系统消息</div>
-								</div>
-							</div>
-						</el-card>
-					</el-col>
-					<el-col :span="8">
-						<el-card shadow="hover" :body-style="{ padding: '0px' }">
-							<div class="grid-content grid-con-3">
-								<el-icon class="grid-con-icon"><Goods /></el-icon>
-								<div class="grid-cont-right">
-									<div class="grid-num">5000</div>
-									<div>商品数量</div>
-								</div>
-							</div>
-						</el-card>
-					</el-col>
-				</el-row>
-				<el-card shadow="hover" style="height: 403px">
-					<template #header>
-						<div class="clearfix">
-							<span>待办事项</span>
-							<el-button style="float: right; padding: 3px 0" text>添加</el-button>
-						</div>
-					</template>
-
-					<el-table :show-header="false" :data="todoList" style="width: 100%">
-						<el-table-column width="40">
-							<template #default="scope">
-								<el-checkbox v-model="scope.row.status"></el-checkbox>
-							</template>
-						</el-table-column>
-						<el-table-column>
-							<template #default="scope">
-								<div
-									class="todo-item"
-									:class="{
-										'todo-item-del': scope.row.status
-									}"
-								>
-									{{ scope.row.title }}
-								</div>
-							</template>
-						</el-table-column>
-					</el-table>
-				</el-card>
-			</el-col>
-		</el-row>
-		<el-row :gutter="20">
-			<el-col :span="12">
-				<el-card shadow="hover">
-					<schart ref="bar" class="schart" canvasId="bar" :options="options"></schart>
-				</el-card>
-			</el-col>
-			<el-col :span="12">
-				<el-card shadow="hover">
-					<schart ref="line" class="schart" canvasId="line" :options="options2"></schart>
+			<el-col :span="24">
+				<el-card shadow="hover" class="mgb20" style="height: 100%">
+          <div id="myChart" :style="{width: '1000px', height: '700px'}"></div>
 				</el-card>
 			</el-col>
 		</el-row>
@@ -120,79 +14,119 @@
 import Schart from 'vue-schart';
 import { reactive } from 'vue';
 import imgurl from '../assets/img/img.jpg';
+import {onMounted} from 'vue';
+// Echarts 为init（dom元素后的类型）
+// EChartsOption 为 option 的类型
+import {ECharts, EChartsOption, init} from 'echarts';
 
 const name = localStorage.getItem('ms_username');
 const role: string = name === 'admin' ? '超级管理员' : '普通用户';
 
-const options = {
-	type: 'bar',
-	title: {
-		text: '最近一周各品类销售图'
-	},
-	xRorate: 25,
-	labels: ['周一', '周二', '周三', '周四', '周五'],
-	datasets: [
-		{
-			label: '家电',
-			data: [234, 278, 270, 190, 230]
-		},
-		{
-			label: '百货',
-			data: [164, 178, 190, 135, 160]
-		},
-		{
-			label: '食品',
-			data: [144, 198, 150, 235, 120]
-		}
-	]
-};
-const options2 = {
-	type: 'line',
-	title: {
-		text: '最近几个月各品类销售趋势图'
-	},
-	labels: ['6月', '7月', '8月', '9月', '10月'],
-	datasets: [
-		{
-			label: '家电',
-			data: [234, 278, 270, 190, 230]
-		},
-		{
-			label: '百货',
-			data: [164, 178, 150, 135, 160]
-		},
-		{
-			label: '食品',
-			data: [74, 118, 200, 235, 90]
-		}
-	]
-};
-const todoList = reactive([
-	{
-		title: '今天要修复100个bug',
-		status: false
-	},
-	{
-		title: '今天要修复100个bug',
-		status: false
-	},
-	{
-		title: '今天要写100行代码加几个bug吧',
-		status: false
-	},
-	{
-		title: '今天要修复100个bug',
-		status: false
-	},
-	{
-		title: '今天要修复100个bug',
-		status: true
-	},
-	{
-		title: '今天要写100行代码加几个bug吧',
-		status: true
-	}
-]);
+onMounted(() => {
+  const labels = ['计算机视觉', '机器人', '大数据'];
+  const course_with_labels = []
+  for(var i = 0;i<labels.length;i++){
+
+  }
+  const myChart = document.getElementById('myChart') as HTMLElement;
+
+  const charEch:ECharts=init(myChart);
+  const option:EChartsOption = {
+    backgroundColor: '#f6f5f3',
+    color: [
+      '#02c9c9',
+      '#bccf3d',
+      '#ffc300'
+    ],
+    title: {
+      text: '课程关系图',
+      textStyle: {
+        color: '#368cbf',
+        fontWeight: 700,
+        fontSize: 30,
+      }
+    }, // 标题及标题颜色、尺寸、位置
+    legend: [ // 增加图示标签，我们可以点击图示隐藏相关节点
+      {
+        show: true,
+        data: labels
+      }],
+    series: [
+      {
+        type: 'graph', // 类型设置为关系图
+        legendHoverLink: true,  // 可以点击图例来隐藏一个组
+        layout: 'force',
+        categories: [
+          {name: '计算机视觉', symbolSize: 60},
+          {name: '机器人', symbolSize: 60},
+          {name: '大数据', symbolSize: 60}
+        ],
+        force: {
+          repulsion: [1000, 1200], //每个节点之间的斥力因子，越大离的越远
+          layoutAnimation: true,
+          friction: 0.3, //刷新时节点的移动速度，越大越快，0 - 1 之间
+          edgeLength: [100, 130] //两节点之间的距离
+        },
+
+        label: {
+          show: true, // 节点圆盘上的文字
+          fontStyle: 'italic', //文字风格，normal，italic，oblique 三种可选
+          fontSize: 12,
+          color: '#000000',
+        },
+        symbolSize: 60, //全局节点尺寸
+        itemStyle: {  // 给节点加上阴影，显着立体
+          shadowColor: '#C0C0C0',
+          shadowOffsetX: 2,
+          shadowOffsetY: 2
+        },
+        //让节点可以通过鼠标拖拽和移动的设置
+        roam: true, //开启鼠标平移及缩放
+        draggable: true, //节点是否支持鼠标拖拽。
+        edgeSymbol: ['circle', 'arrow'],//两节点连线的样式
+        edgeSymbolSize: [5, 10],
+        cursor: 'pointer', //鼠标悬浮时在图形元素上时鼠标的样式
+        labelLayout: {
+          moveOverlap: 'shiftX', //标签重叠时，挪动标签防止重叠
+          draggable: true //节点标签是否允许鼠标拖拽定位
+        },
+        emphasis: {
+          scale: true, //节点放大效果
+          focus: 'adjacency'
+        },
+        lineStyle: {
+          color: '#3d3d3f',
+          width: 2,
+          curveness: 0 //节点连线的曲率，0-1 越大越弯。
+        },
+
+        data: [
+          {
+            name: '机器学习与人工智能',
+            symbolSize: 80,
+            category: '计算机视觉'
+          },
+          {
+            name: '图解深度学习',
+            category: '计算机视觉'
+          },
+
+
+        ],
+        links: [
+          {
+            source:'机器学习与人工智能',
+            target:'图解深度学习'
+          }
+        ]
+      }
+    ]
+  };
+  charEch.setOption(option)
+  // 绘制图表
+
+});
+
 </script>
 
 <style scoped>
